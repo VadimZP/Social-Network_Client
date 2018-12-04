4249e59bae3e1d76344a1fda5befb4abebb03d91
Vadim <wesborlandzp@gmail.com>
2018-12-04 14:13:42 +0200

    Create socket.js

---
+++ src/socket.js
@@ -0,0 +1,16 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+cosnt port = process.env.PORT || 8080
+
+io.on('connection', socket => {
+    socket.on('appendMessage', message => {
+        io.emit(message.receiver_id, {type: 'message', ...message})
+        io.emit(message.sender_id, {type: 'message', ...message})
+    })
+    socket.on('appendNotification', notification => {
+        io.emit(notification.receiver_id, {type: 'notification', ...notification})
+    })
+})
+
+http.listen(port, () => console.log(`listening on *:${port}`))
